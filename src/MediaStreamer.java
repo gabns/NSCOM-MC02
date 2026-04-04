48f28205bebcc5481168717dc701a94ce483a07e
gabns <gabby.abenes@gmail.com>
2026-04-04 18:58:56 +0800

    implemented streamer and client

--- src/MediaStreamer.java
+++ src/MediaStreamer.java
@@ -1,16 +1,92 @@
 import java.io.*;
 import java.net.*;
+import javax.sound.sampled.*;
 
 public class MediaStreamer {
-    // Rubric: Sends audio frames from a file in a timely manner 
-    public void startSender(File audioFile, InetAddress dest, int port) {
-        // TODO: Read file (.wav or G.711) 
-        // TODO: Packetize and send via UDP at 20ms intervals 
+
+    private boolean isRunning = true;
+
+    // Call this from your main class to cleanly stop the threads when the call ends
+    public void stopStreaming() {
+        isRunning = false;
     }
 
-    // Rubric: Accurately receives and plays audio 
+    public void startSender(File audioFile, InetAddress dest, int port) {
+        new Thread(() -> {
+            try (FileInputStream fis = new FileInputStream(audioFile);
+                 DatagramSocket rtpSocket = new DatagramSocket()) {
+
+                System.out.println("RTP Sender Thread Started. Streaming to port: " + port);
+                
+                // 160 bytes = 20ms of 8kHz 8-bit mono audio (G.711 PCMU)
+                byte[] buffer = new byte[160];
+                int seqNum = 0;
+                int timestamp = 0;
+                int ssrc = 12345;
+
+                while (isRunning && fis.read(buffer) != -1) {
+                    // Create the packet using your partner's class
+                    RtpPacket rtp = new RtpPacket(seqNum, timestamp, ssrc, buffer);
+                    byte[] packetData = rtp.toNetworkBytes();
+
+                    // Send the packet
+                    DatagramPacket packet = new DatagramPacket(packetData, packetData.length, dest, port);
+                    rtpSocket.send(packet);
+
+                    seqNum++;
+                    timestamp += 160;
+
+                    // Crucial for the rubric: "Timely manner". 
+                    // Sleep for 20ms so it plays at normal speed.
+                    Thread.sleep(20);
+                }
+                
+                System.out.println("File stream finished.");
+
+            } catch (Exception e) {
+                System.out.println("Sender Error: " + e.getMessage());
+            }
+        }).start();
+    }
+
     public void startReceiver(int localPort) {
-        // TODO: Listen for RTP packets 
-        // TODO: Strip RTP header and send payload to audio player 
+        new Thread(() -> {
+            try (DatagramSocket rtpSocket = new DatagramSocket(localPort)) {
+                
+                System.out.println("RTP Receiver Thread Started. Listening on port: " + localPort);
+
+                // Setup Java Audio System for G.711 (PCMU) playback
+                AudioFormat format = new AudioFormat(8000, 8, 1, true, false);
+                DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+                
+                if (!AudioSystem.isLineSupported(info)) {
+                    System.out.println("Audio line not supported.");
+                    return;
+                }
+
+                SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+                line.open(format);
+                line.start();
+
+                // 12 bytes for header + 160 for payload
+                byte[] buffer = new byte[172]; 
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                while (isRunning) {
+                    rtpSocket.receive(packet);
+
+                    // The payload data is the packet length minus the 12-byte header.
+                    // We write directly to the speakers, offsetting by 12 bytes to skip the header.
+                    int payloadSize = packet.getLength() - 12;
+                    line.write(packet.getData(), 12, payloadSize);
+                }
+
+                line.drain();
+                line.close();
+
+            } catch (Exception e) {
+                System.out.println("Receiver Error: " + e.getMessage());
+            }
+        }).start();
     }
 }
--- src/SipClient.java
+++ src/SipClient.java
@@ -2,24 +2,96 @@ import java.net.*;
 
 public class SipClient {
     private DatagramSocket socket;
+    private InetAddress remoInetAddress;
+    private int remotePort;
     
+    
+    public SipClient(int localPort) throws SocketException{
+        this.socket = new DatagramSocket(localPort);
+    }
     // Rubric: Includes all required SIP headers 
-    public void sendInvite(String destinationIp, int port, String sdpData) {
+    public void sendInvite(String destinationIp, int port, String sdpData) throws Exception {
+        this.remoInetAddress = InetAddress.getByName(destinationIp);
+        this.remotePort = port;
+        
         // TODO: Construct the SIP INVITE string [cite: 23, 44]
+        String sdp = "v=0\r\n" +
+                     "o=- 0 0 IN IP4 " + InetAddress.getLocalHost().getHostAddress() + "\r\n" +
+                     "s=Sip Call\r\n" +
+                     "c=IN IP4 " + InetAddress.getLocalHost().getHostAddress() + "\r\n" +
+                     "t=0 0\r\n" +
+                     "m=audio " + port + " RTP/AVP 0\r\n" +
+                     "a=rtpmap:0 PCMU/8000\r\n";
         // TODO: Ensure SDP is correctly embedded in the body [cite: 45, 81]
+        String invite = "INVITE sip:" + destinationIp + ":" + port + " SIP/2.0\r\n" +
+                        "Via: SIP/2.0/UDP " + InetAddress.getLocalHost().getHostAddress() + ":" + socket.getLocalPort() + "\r\n" +
+                        "From: <sip:client@" + InetAddress.getLocalHost().getHostAddress() + ">;tag=12345\r\n" +
+                        "To: <sip:" + destinationIp + ":" + port + ">\r\n" +
+                        "Call-ID: 1234567890@client\r\n" +
+                        "CSeq: 1 INVITE\r\n" +
+                        "Content-Type: application/sdp\r\n" +
+                        "Content-Length: " + sdp.length() + "\r\n\r\n" +
+                        sdp;
+
+        byte[] msg = invite.getBytes();
+        socket.send(new DatagramPacket(msg, msg.length, remoInetAddress, remotePort));
+        System.out.println("Sent SIP INVITE to " + destinationIp + ":" + port);
     }
 
-    public void processResponse() {
-        // TODO: Receive packet and parse for "200 OK" [cite: 31, 81]
-        // TODO: Parse remote IP/Port/Codec from the SDP body 
+    public int processResponse() throws Exception {
+        byte[] buffer = new byte[2048];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+        System.out.println("Waiting for SIP response...");
+        socket.receive(packet);
+        String response = new String(packet.getData(), 0, packet.getLength());
+
+        if(response.contains("200 OK")) {
+            System.out.println("Received 200 OK from " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
+            String targetPortString = "m=audio ";
+            int idx = response.indexOf(targetPortString);
+            if (idx != -1) {
+                int startIdx = idx + targetPortString.length();
+                int endIdx = response.indexOf(" ", startIdx);
+
+                String portStr = response.substring(startIdx, endIdx);
+                System.out.println("Parsed remote RTP port: " + portStr);
+                return Integer.parseInt(portStr);
+        } 
+    }
+    else if(response.contains("SIP/2.0 4") || response.contains("SIP/2.0 5")) {
+            System.out.println("Received unexpected response: " + response);
+        }
+    return -1; // Indicate failure to parse port
+}
+    
+
+    public void sendAck() throws Exception {
+        String ack = "ACK sip:" + remoInetAddress.getHostAddress() + ":" + remotePort + " SIP/2.0\r\n" +
+                     "Via: SIP/2.0/UDP " + InetAddress.getLocalHost().getHostAddress() + ":" + socket.getLocalPort() + "\r\n" +
+                     "From: <sip:client@" + InetAddress.getLocalHost().getHostAddress() + ">;tag=12345\r\n" +
+                     "To: <sip:" + remoInetAddress.getHostAddress() + ":" + remotePort + ">\r\n" +
+                     "Call-ID: 1234567890@client\r\n" +
+                     "CSeq: 1 ACK\r\n\r\n";
+
+        byte[] msg = ack.getBytes();
+        socket.send(new DatagramPacket(msg,msg.length, remoInetAddress, remotePort));
+        System.out.println("Sent SIP ACK to " + remoInetAddress.getHostAddress() + ":" + remotePort);
     }
 
-    public void sendAck() {
-        // TODO: Send the ACK to confirm call establishment [cite: 34, 44]
-    }
+    // Rubric: Uses BYE message to gracefully end the call 
+    public void sendBye() throws Exception {
 
-    // Rubric: Uses BYE message to gracefully end the call [cite: 37, 46, 81]
-    public void sendBye() {
+        String bye = "BYE sip:" + remoInetAddress.getHostAddress() + ":" + remotePort + " SIP/2.0\r\n" +
+                     "Via: SIP/2.0/UDP " + InetAddress.getLocalHost().getHostAddress() + ":" + socket.getLocalPort() + "\r\n" +
+                     "From: <sip:client@" + InetAddress.getLocalHost().getHostAddress() + ">;tag=12345\r\n" +
+                     "To: <sip:" + remoInetAddress.getHostAddress() + ":" + remotePort + ">\r\n" +
+                     "Call-ID: 1234567890@client\r\n" +
+                     "CSeq: 2 BYE\r\n\r\n";
         // TODO: Send BYE and close sockets/free resources 
+        byte[] msg = bye.getBytes();
+        socket.send(new DatagramPacket(msg, msg.length, remoInetAddress, remotePort));
+        System.out.println("Sent SIP BYE to " + remoInetAddress.getHostAddress() + ":" + remotePort);
+        socket.close();
     }
 }
